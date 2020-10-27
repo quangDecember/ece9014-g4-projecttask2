e71f278c8a74d8bbae28ccdbeb88a0baa93cb728
fhossa5 <71348613+fhossa5@users.noreply.github.com>
2020-10-27 08:41:28 +0300

    Added User, Kernel, Team, Org and related tables
    Added create statements for:
    USERS
    LANGUAGES
    ORGANIZATIONS
    TEAMS
    KERNELS
    KERNEL_VERSIONS
    KERNEL_VERSION_DATASET_SOURCES
    KERNEL_TAGS
    KERNEL_VOTES
    USER_ACHIEVEMENTS
    TEAM_MEMBERSHIPS
    SUBMISSIONS
    USER_ORGANIZATIONS
    USER_FOLLOWERS

--- sql worksheet project_v3.sql
+++ sql worksheet project_v3.sql
@@ -160,3 +160,207 @@ CompetitionId INT NOT NULL references COMPETITION(CompetitionId),
 TagId INT not null references tags_TB(tagid),
 PRIMARY KEY (competitionTagId,CompetitionId,TagId)
 );
+
+
+CREATE TABLE USERS (
+    userId	INT	NOT NULL,
+    UserName	NVARCHAR2 (50) ,
+    DisplayName	VARCHAR2 (50) ,
+    RegisterDate	DATE default sysdate NOT NULL,
+    PerformanceTier	NUMBER (2)	,
+    CONSTRAINT user_PK PRIMARY KEY (userId)  
+);
+
+
+CREATE TABLE LANGUAGES (
+    languageId INT NOT NULL,
+    languageName VARCHAR2 (30) NOT NULL,
+    DisplayName	VARCHAR2 (10) NOT NULL,
+    IsNotebook NUMBER(1) default 0 NOT NULL,
+    CONSTRAINT language_PK PRIMARY KEY (languageId),
+    CONSTRAINT language_BOOL_IsNotebook CHECK (IsNotebook IN (0,1))
+);
+
+
+CREATE TABLE ORGANIZATIONS (
+    orgId INT NOT NULL,
+    orgName	NVARCHAR2 (50) NOT NULL,
+    Slug VARCHAR2 (20) NOT NULL,
+    CreationDate DATE  default sysdate NOT NULL,
+    Description NVARCHAR2 (500),
+    CONSTRAINT organization_PK PRIMARY KEY (orgId)
+);
+
+CREATE TABLE TEAMS (
+     teamId	INT	NOT NULL,
+    CompetitionId	INT	NOT NULL,
+    TeamLeaderId	INT	,
+    TeamName	NVARCHAR2 (50)	,
+    ScoreFirstSubmittedDate	DATE	, --should be derived but insufficient data available
+    --LastSubmissionDate	DATE	, --[DERIVED]
+    PublicLeaderboardSubmissionId	INT	,
+    PrivateLeaderboardSubmissionId	INT	,
+    IsBenchmark	NUMBER (1)	,
+    Medal	NUMBER (1)	,
+    MedalAwardDate	DATE	,
+    PublicLeaderboardRank	INT	,
+    PrivateLeaderboardRank	INT	,
+    CONSTRAINT team_PK PRIMARY KEY (teamId) ,
+    CONSTRAINT team_FK_leader FOREIGN KEY (TeamLeaderId) REFERENCES USERS(userId) ,
+    CONSTRAINT team_FK_Competition FOREIGN KEY (CompetitionId) REFERENCES COMPETITION(CompetitionId),
+    CONSTRAINT team_BOOL_IsBenchmark CHECK (IsBenchmark is null or IsBenchmark IN (0,1))
+); 
+
+CREATE TABLE KERNELS (
+     scriptId	INT 	NOT NULL,
+    AuthorId	INT 	NOT NULL,
+    CurrentKernelVersionId	INT 	,
+    ForkParentKernelVersionId	INT 	,
+    ForumTopicId	INT 	,
+    FirstKernelVersionId	INT 	,
+    CreationDate	DATE	,
+    EvaluationDate	DATE	,
+    MadePublicDate	DATE	,
+    IsProjectLanguageTemplate	NUMBER (1)	,
+    CurrentUrlSlug	VARCHAR2 (20)	,
+    Medal	NUMBER (1)	,
+    MedalAwardDate	DATE	,
+    
+    /* DERIVED:
+    TotalViews	INT 	,
+    TotalComments	INT 	,
+    TotalVotes	INT 	,
+    */
+    
+    CONSTRAINT kernel_PK PRIMARY KEY (scriptId) ,    
+    CONSTRAINT kernel_FK_Author FOREIGN KEY (AuthorId) REFERENCES USERS(userId) ,
+    CONSTRAINT kernel_BOOL_IsProjectLanguageTemplate 
+        CHECK (IsProjectLanguageTemplate is null or IsProjectLanguageTemplate IN (0,1))
+);
+
+CREATE TABLE KERNEL_VERSIONS (
+ 	kvId	INT	NOT NULL,	
+	ScriptId	INT	NOT NULL,	
+	ParentKVID	INT	,	
+	languageId	INT	NOT NULL,	
+	AuthorId	INT	NOT NULL,	
+	CreationDate	DATE default sysdate NOT NULL,	
+	VersionNumber	INT	,	
+	Title	VARCHAR2 (50)	,	
+	EvaluationDate	DATE	,	
+	IsChange	NUMBER(1)	,	
+	TotalLines	INT	,	
+	LinesInsertedFromPrevious	INT	,	
+	LinesChangedFromPrevious	INT	,	
+	LinesUnchangedFromPrevious	INT	,	
+	LinesInsertedFromFork	INT	,	
+	LinesDeletedFromFork	INT	,	
+	LinesChangedFromFork	INT	,	
+	LinesUnchangedFromFork	INT	,	
+	--TotalVotes	INT	,	  --[DERIVED]
+    CONSTRAINT kernelVersions_PK PRIMARY KEY (kvId) ,    
+    CONSTRAINT kernelVersions_FK_Author FOREIGN KEY (AuthorId) REFERENCES USERS(userId) ,
+    CONSTRAINT kernelVersions_FK_Parent FOREIGN KEY (ParentKVID) REFERENCES KERNEL_VERSIONS(kvId) ,
+    CONSTRAINT kernelVersions_FK_Language FOREIGN KEY (languageId) REFERENCES LANGUAGES(languageId) ,
+    CONSTRAINT kernelVersions_BOOL_IsChange CHECK (IsChange is null or IsChange IN (0,1))
+);
+
+CREATE TABLE KERNEL_VERSION_DATASET_SOURCES (
+    kvdsId INT NOT NULL,
+    kvId INT NOT NULL,
+    datasetvid INT NOT NULL,
+    CONSTRAINT KERNEL_VERSION_DATASET_SOURCES_PK PRIMARY KEY (kvdsId) ,
+    CONSTRAINT KERNEL_VERSION_DATASET_SOURCES_FK_kv FOREIGN KEY (kvId) REFERENCES KERNEL_VERSIONS(kvId) ,
+    CONSTRAINT KERNEL_VERSION_DATASET_SOURCES_FK_datasetvid FOREIGN KEY (datasetvid) REFERENCES datasetversions_TB(datasetvid) ,
+    CONSTRAINT KERNEL_VERSION_DATASET_SOURCES_UNIQUE UNIQUE(kvId,datasetvid) 
+);
+
+CREATE TABLE KERNEL_TAGS (
+    kernelTagId INT NOT NULL,
+    scriptId INT NOT NULL,
+    tagid INT NOT NULL,
+    CONSTRAINT KERNEL_TAGS_PK PRIMARY KEY (kernelTagId) ,
+    CONSTRAINT KERNEL_TAGS_FK_kv FOREIGN KEY (scriptId) REFERENCES KERNELS(scriptId) ,
+    CONSTRAINT KERNEL_TAGS_FK_tag FOREIGN KEY (tagid) REFERENCES tags_TB(tagid) ,
+    CONSTRAINT KERNEL_TAGS_UNIQUE UNIQUE(scriptId,tagid) 
+);
+
+CREATE TABLE KERNEL_VOTES (
+    kernelVoteId INT NOT NULL,
+    kvId INT NOT NULL,
+    userId INT NOT NULL,
+    CONSTRAINT kernelVote_PK PRIMARY KEY (kernelVoteId) ,
+    CONSTRAINT kernelVote_FK_kv FOREIGN KEY (kvId) REFERENCES KERNEL_VERSIONS(kvId) ,
+    CONSTRAINT kernelVote_FK_voter FOREIGN KEY (userId) REFERENCES USERS(userId) ,
+    CONSTRAINT kernelVote_UNIQUE UNIQUE(kvId,userId) 
+);
+
+CREATE TABLE USER_ACHIEVEMENTS (
+	achievementId	INT	NOT NULL,	
+	userId	INT	NOT NULL,	
+	AchievementType	VARCHAR2 (20)	NOT NULL,	
+	AchievementTier	NUMBER (1)	NOT NULL,	
+	TierAchievementDate	DATE	,	
+	Points	INT	NOT NULL,
+    
+    --should technically be derived, but we have insufficient data
+	CurrentRanking	INT	,	
+	HighestRanking	INT	,	
+	TotalGold	INT	NOT NULL,	
+	TotalSilver	INT	NOT NULL,	
+	TotalBronze	INT	NOT NULL,
+    
+    CONSTRAINT USER_ACHIEVEMENTS_PK PRIMARY KEY (achievementId) ,
+    CONSTRAINT USER_ACHIEVEMENTS_FK_user FOREIGN KEY (userId) REFERENCES USERS(userId) 
+);
+
+CREATE TABLE TEAM_MEMBERSHIPS (
+    membershipId INT NOT NULL,
+    teamId INT NOT NULL,
+    userId INT NOT NULL,
+    requestDate DATE default sysdate NOT NULL,
+    CONSTRAINT TEAM_MEMBERSHIPS_PK PRIMARY KEY (membershipId) ,
+    CONSTRAINT TEAM_MEMBERSHIPS_FK_team FOREIGN KEY (teamId) REFERENCES TEAMS(teamId) ,
+    CONSTRAINT TEAM_MEMBERSHIPS_FK_member FOREIGN KEY (userId) REFERENCES USERS(userId) ,
+    CONSTRAINT TEAM_MEMBERSHIPS_UNIQUE UNIQUE(teamId,userId) 
+);
+
+CREATE TABLE SUBMISSIONS (
+    submissionId	INT	NOT NULL,	
+	userId	INT	,	
+	teamId	INT	NOT NULL,	
+	SourceKernelVersionId	INT	,	
+	SubmissionDate	DATE	default sysdate NOT NULL,	
+	ScoreDate	DATE	,	
+	--IsAfterDeadline	NUMBER (1)	NOT NULL,	[DERIVED]
+	PublicScoreLeaderboardDisplay	NUMBER (5,5)	,	
+	PublicScoreFullPrecision	NUMBER	,	
+	PrivateScoreLeaderboardDisplay	NUMBER (5,5)	,	
+	PrivateScoreFullPrecision	NUMBER	,	
+    CONSTRAINT submissions_PK PRIMARY KEY (submissionId) ,
+    CONSTRAINT submissions_FK_team FOREIGN KEY (teamId) REFERENCES TEAMS(teamId) ,
+    CONSTRAINT submissions_FK_userId FOREIGN KEY (userId) REFERENCES USERS(userId) ,
+    CONSTRAINT submissions_FK_kvId FOREIGN KEY (SourceKernelVersionId) REFERENCES KERNEL_VERSIONS(kvId)
+);
+
+CREATE TABLE USER_ORGANIZATIONS (
+    orgMemberId INT NOT NULL,
+    orgId INT NOT NULL,
+    userId INT NOT NULL,
+    CONSTRAINT UserOrganizations_PK PRIMARY KEY (orgMemberId) ,
+    CONSTRAINT UserOrganizations_FK_org FOREIGN KEY (orgId) REFERENCES ORGANIZATIONS(orgId) ,
+    CONSTRAINT UserOrganizations_FK_member FOREIGN KEY (userId) REFERENCES USERS(userId) ,
+    CONSTRAINT UserOrganizations_UNIQUE UNIQUE(orgId,userId) 
+);
+
+CREATE TABLE USER_FOLLOWERS (
+    followId INT NOT NULL,
+    userId INT NOT NULL,
+    followingUserId INT NOT NULL,
+    creationDate DATE default sysdate NOT NULL,
+    CONSTRAINT UserFollowers_PK PRIMARY KEY (followId) ,
+    CONSTRAINT UserFollowers_FK_user FOREIGN KEY (userId) REFERENCES USERS(userId) ,
+    CONSTRAINT UserFollowers_FK_follower FOREIGN KEY (followingUserId) REFERENCES USERS(userId) ,
+    CONSTRAINT UserFollowers_UNIQUE UNIQUE(userId,followingUserId) 
+);
+
