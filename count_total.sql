b7046b7ebd7a452af35d1b87fd6b825bc35abe66
Quang <quangDecember@gmail.com>
2020-11-21 02:06:09 +0800

    The total number of medals won by a user for their entire kaggle history.

--- count_total.sql
+++ count_total.sql
@@ -5,3 +5,7 @@ group by USERID;
 select count(forummid),userid
 from forummessages_tb
 group by userid;
+
+select sum(totalgold + totalsilver + totalbronze), userid
+from user_achievements
+group by userid;
