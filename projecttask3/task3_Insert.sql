e782a62868cdb0cf2ab75ed6839326560a475fa5
fhossa5 <71348613+fhossa5@users.noreply.github.com>
2020-11-21 06:43:46 +0200

    Insert statements
    (datasets not included in insertion to fact table)

--- projecttask3/task3_Insert.sql
+++ projecttask3/task3_Insert.sql
@@ -1,45 +1,58 @@
-INSERT INTO DW.Competition(Title, TeamID, EnabledDate, DeadlineDate, OnlyAllowKernelSubmissions, TeamLeader)
+INSERT INTO DW.Competition(Title, TeamID, EnabledDate, DeadlineDate, OnlyAllowKernelSubmissions, TeamLeader,CompetitionId)
     (
-        SELECT c.Title, t.teamID, c.EnabledDate, c.DeadlineDate, c.OnlyAllowKernelSubmissions, t.TeamLeader
+        SELECT c.Title, t.teamID, c.EnabledDate, c.Deadline, c.OnlyAllowKernelSubmission, t.TeamLeader , c.CompetitionId
         FROM op.COMPETITION c, op.TEAMS t
         WHERE
             c.CompetitionId = t.CompetitionId
-    )
+    );
 
-INSERT INTO DW.Dataset(Title, Subtitle, OwnerUserId, CurrentDatasetVersionId)
+
+
+INSERT INTO DW.Dataset(id,Title, Subtitle, OwnerUserId, CurrentDatasetVersionId)
     (
-        SELECT Title, Subtitle, OwnerUserId, CurrentDatasetVersionId
+        SELECT datasetid, Title, Subtitle, OwnerUserId, CurrentDatasetVersionId
         FROM op.datasets_TB
-    )
+        WHERE OwnerUserId is not null
 
-INSERT INTO DW.USER(UserName, DisplayName, RegisterDate, PerformanceTier)
+    );
+
+INSERT INTO DW.USERS(id,UserName, DisplayName, RegisterDate, PerformanceTier)
     (
-        SELECT UserName, DisplayName, RegisterDate, PerformanceTier
+        SELECT userId, UserName, DisplayName, RegisterDate, PerformanceTier
         FROM op.USERS
-    )
+    );
+      	
 
-INSERT INTO DW.FORUM(Title, Creationdate, TopicID)
+INSERT INTO DW.FORUM(FORUMID, Title, Creationdate, TopicID)
     (
-        SELECT f.Title, ft.Creationdate, ft.Forumtid
+        SELECT f.ForumID, f.Title, ft.Creationdate, ft.Forumtid
         FROM op.forum_TB f, op.forumtopics_TB ft
         WHERE
             f.ForumID = ft.forumid
-    )
+    );
 
-INSERT INTO DW.KERNEL(CreationDate, AuthorUserID, CurrentKernelVersionID, MadePublicDate)
+INSERT INTO DW.KERNEL(id, CreationDate, AuthorUserID, CurrentKernelVersionID, MadePublicDate)
     (
-        SELECT CreationDate, AuthorId, CurrentKernelVersionId, MadePublicDate
+        SELECT scriptId, CreationDate, AuthorId, CurrentKernelVersionId, MadePublicDate
         FROM op.KERNELS
     )
 
-INSERT INTO DW.PERFORMANCE_FACT(UsersID, CompetitionsID, DatasetsID, ForumsID, KernelsID, TotalSubmissions, TotalMedals, TotalForumMessages)
+INSERT INTO DW.PERFORMANCE_FACT(UsersID, CompetitionsID, ForumsID, KernelsID, TotalSubmissions, AvgScore, TotalMedals, TotalForumMessages)
     (
-        SELECT dw_u.ID, dw_c.ID, dw_d.ID, dw_f.ID, dw_k.ID, COUNT(s.submissionId), SUM(a.TotalGold)+SUM(a.TotalSilver)+SUM(a.TotalBronze), COUNT(fm.ForumMID)
-        FROM DW.COMPETITION dw_c, DW.DATASET dw_d, DW.USER dw_u, DW.FORUM dw_f, DW.KERNEL dw_k, op.SUBMISSIONS s, op.USER_ACHIEVEMENTS a, op.forummessages_TB fm
-        WHERE
-            dw_u.UserId = s.userId
-        and dw_u.UserId = fm.UserId
-        and dw.u.UserId = a.userId
-        GROUP BY
-            dw.u.UserId
+        select AA.user, AA.competition, BB.Forum, AA.kernel, AA.TotalSubmissions, AA.avgScore, BB.TotalMedals, BB.TotalMessages
+        from 
+        (
+            select count(*) as TotalSubmissions , U.id as user, C.id as competition , K.id and kernel , AVG(S.PublicScoreFullPrecision) as avgScore
+            FROM DW.USERS U, DW.Competition C, DW.KERNEL K, OP.SUBMISSIONS S, KERNEL_VERSIONS V
+            where  U.id = S.userId and C.TeamID = S.teamId and S.SourceKernelVersionId = V.kvId and V.ScriptId = K.id 
+            group by U.id , C.id , K.id
+        )AA
+        LEFT JOIN 
+        (
+            select COUNT(*) as TotalMessages , SUM(M.Medal) as TotalMedals , MAX(F.id) as Forum , C.id, U.id
+            FROM DW.USERS U, DW.Competition C, DW.FORUM F, OP.COMPETITION O, OP.forummessages_TB M
+            where C.CompetitionId = O.CompetitionId  AND O.ForumID=F.FORUMID AND F.TopicID=M.forumTID AND M.UserId=U.id
+            group by C.id, U.id
+        )BB
+        ON AA.competition = BB.competition AND AA.user = BB.user
     )
